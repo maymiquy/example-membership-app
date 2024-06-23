5301c3ae3fd87d232d146a067f22300236453d4f
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2024-06-24 01:51:26 +0700

    feat: login with regular authentication

--- client/src/components/Auhtentication/Modal/LoginModal.jsx
+++ client/src/components/Auhtentication/Modal/LoginModal.jsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
+import React, { useState } from "react";
+import axios from "axios";
 import {
  Dialog,
  DialogContent,
@@ -15,12 +14,35 @@ import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 
 const LoginModal = () => {
+ const [email, setEmail] = useState("");
+ const [password, setPassword] = useState("");
+ const [errorMessage, setErrorMessage] = useState("");
+
+ const handleLogin = async () => {
+  try {
+   const response = await axios.post("http://localhost:5000/api/login", {
+    email,
+    password,
+   });
+
+   console.log(response.data);
+
+   const { token } = response.data;
+
+   localStorage.setItem("authToken", token);
+   axios.defaults.headers.common["authorization"] = `Bearer ${token}`;
+
+   window.location.reload();
+  } catch (error) {
+   console.error("Login failed:", error.message);
+   setErrorMessage("Invalid credentials");
+  }
+ };
+
  return (
   <Dialog>
    <DialogTrigger asChild>
-    <Button variant="default" asChild>
-     <Link>Login</Link>
-    </Button>
+    <Button variant="default">Login</Button>
    </DialogTrigger>
    <DialogContent>
     <DialogHeader>
@@ -32,13 +54,33 @@ const LoginModal = () => {
     <div className="grid gap-4 py-4">
      <div className="grid gap-2">
       <Label htmlFor="email">Email</Label>
-      <Input id="email" type="email" placeholder="name@example.com" />
+      <Input
+       id="email"
+       type="email"
+       placeholder="name@example.com"
+       value={email}
+       onChange={(e) => setEmail(e.target.value)}
+      />
+      {errorMessage && (
+       <p className="text-red-500 text-xs truncate">{errorMessage}</p>
+      )}
      </div>
      <div className="grid gap-2">
       <Label htmlFor="password">Password</Label>
-      <Input id="password" type="password" placeholder="*******" />
+      <Input
+       id="password"
+       type="password"
+       placeholder="*******"
+       value={password}
+       onChange={(e) => setPassword(e.target.value)}
+      />
+      {errorMessage && (
+       <p className="text-red-500 text-xs truncate">{errorMessage}</p>
+      )}
      </div>
-     <Button variant="default">Login</Button>
+     <Button variant="default" onClick={handleLogin}>
+      Login
+     </Button>
     </div>
     <DialogFooter className="items-center justify-center"></DialogFooter>
    </DialogContent>
