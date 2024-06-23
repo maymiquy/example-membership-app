082bba52e7c1e1e3b395e7c8c9aa2dc53aae5745
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2024-06-24 01:58:26 +0700

    fix: missing adding protected route at outlet router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import WelcomePage from "./pages/WelcomePage";
 import SubscriptionPage from "./pages/Subscription/SubscriptionPage";
+import ProtectedRoute from "./ProtectedRoutes";
 
 function App() {
  return (
   <Routes>
    <Route path="/" element={<WelcomePage />} />
-   <Route path="/subscription" element={<SubscriptionPage />} />
+   <Route element={<ProtectedRoute />}>
+    <Route path="/subscription" element={<SubscriptionPage />} />
+   </Route>
   </Routes>
  );
 }
