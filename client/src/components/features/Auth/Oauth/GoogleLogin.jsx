8dd4d834de8f0b3047693812028d319ba2133238
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2024-08-29 16:56:40 +0700

    fix: handle twice request to server on oaut google login

--- client/src/components/features/Auth/Oauth/GoogleLogin.jsx
+++ client/src/components/features/Auth/Oauth/GoogleLogin.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../../../ui/button";
 import { GrGoogle } from "react-icons/gr";
 import { useGoogleLogin } from "@react-oauth/google";
 import { oauthGoogle, storeToken } from "../../../../services/auth.service";
 import { toast } from "../../../ui/use-toast";
 import { useUserContext } from "../../../../hooks/useUserContext";
+import Spinner from "../../../common/Spinner";
 
 const GoogleLogin = () => {
  const { setUser } = useUserContext();
+ const [loading, setLoading] = useState(false);
 
  const handleGoogleLogin = useGoogleLogin({
   onSuccess: async (tokenResponse) => {
@@ -17,35 +19,62 @@ const GoogleLogin = () => {
     if (token) storeToken(token);
     setUser(user);
 
+    setLoading(false);
     toast({
      title: "Login Successful",
     });
    } catch (error) {
     console.error("Error during google login:", error.message);
+    setLoading(false);
    }
   },
   onError: (error) => {
    console.error("Google login failed:", error);
    const messageError = error.response.data;
-
+   setLoading(false);
    toast({
     title: "Login Failed",
     description: messageError.error || messageError.errors[0].msg,
     variant: "destructive",
    });
   },
+  onNonOAuthError: () => {
+   setLoading(false);
+  },
  });
+
+ const handleClick = (event) => {
+  if (loading) {
+   event.preventDefault();
+   event.stopPropagation();
+  } else {
+   setLoading(true);
+   handleGoogleLogin();
+  }
+ };
 
  return (
   <Button
+   disabled={loading}
+   loading={loading}
    variant="outline"
-   className="w-[170px] flex flex-row justify-start"
-   onClick={() => handleGoogleLogin()}
+   className={`w-[170px] flex flex-row ${
+    loading ? "justify-center" : "justify-start"
+   } `}
+   onClick={handleClick}
    asChild
   >
-   <span className="gap-2 cursor-pointer">
-    <GrGoogle size="18px" />
-    Login Google
+   <span className={`gap-2 ${loading ? "cursor-progress" : "cursor-pointer"}`}>
+    {loading ? (
+     <>
+      <Spinner size="small" />
+     </>
+    ) : (
+     <>
+      <GrGoogle size="18px" />
+      Login Google
+     </>
+    )}
    </span>
   </Button>
  );
